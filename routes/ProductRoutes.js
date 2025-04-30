ffd616250826d3955bae65c9b905eb8c1df4f34d
Kotoka Emmanuel Yayra Yaw <emmanuelyayrakotoka@gmail.com>
2025-04-30 16:35:48 +0000

    Still

--- routes/ProductRoutes.js
+++ routes/ProductRoutes.js
@@ -18,7 +18,7 @@ router.get('/products', authenticateUser, getAllProducts);        // Get all pro
 router.get('/products/:id', authenticateUser, getProductById);    // Get single product
 router.put('/products/:id', authenticateUser, updateProduct);     // Update a product
 router.delete('/products/:id', authenticateUser, deleteProduct);  // Delete a product
-router.put('/products/bulk', authenticateUser, bulkWriteProducts); // Bulk write products
+router.post('/products/bulk', authenticateUser, bulkWriteProducts); // Bulk write products
 router.get('/products/advanced', authenticateUser, advancedQueryProducts); // Advanced query for products
 
 module.exports = router;
